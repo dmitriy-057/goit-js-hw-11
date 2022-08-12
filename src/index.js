d4f99039524f79bf61dd89a1f4109e139d595734
Dmitriy Gonharov <my.webpage057@gmail.com>
2022-08-12 23:37:31 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox"; 
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// подключение библиотеки SimpleLightbox
+let gallery = new SimpleLightbox(".gallery a", {
+  captions:true,
+  captionSelector: "img",
+  // captionsData: "alt",
+  // captionPosition: "bottom",
+  // captionDelay: 250,
+  enableKeyboard: true,
+});
 
 const refsEl ={
   form: document.querySelector(".search-form"),
@@ -10,49 +20,70 @@ const refsEl ={
   galleryContainer: document.querySelector(".gallery")
 }
 
-const getPosts = async (e) => {
+refsEl.getBtn.addEventListener("click", sumbitSurch);
+
+function sumbitSurch(e) {
   e.preventDefault()
+  getPosts()
+}
+
+const getPosts = async () => {
+  // e.preventDefault()
   try {
-    const response = await axios.get(`https://pixabay.com/api/?key=29210870-5c756012ce316252fd55732c8&q=${refsEl.input.value}&image_type=photo&orientation=horizontal&safesearch=true`);
-    createImagesCardsMarkup(response.galleryItems)
+    const response = await axios.get(`https://pixabay.com/api/?key=29209271-716f3ea82b952e36eef48fa19&q=${refsEl.input.value}&image_type=photo&orientation=horizontal&safesearch=true`);
+    
+    createList(response.data.hits);
+    
+  
+    // createImagesCardsMarkup(response.galleryItems)
   } catch (error) {
     console.log(error);
   }
+};
+
+// getPosts()
+
+
+function createList(data) {
+  const result = data.map(({ webformatURL,  largeImageURL, tags, likes, views, comments, downloads}) => {
+    return `
+    <div class="img-thumb">
+    <a href="${webformatURL}">
+  <img  src="${largeImageURL}" alt="${tags }  loading="lazy" 
+  />
+  </a>
+
+<div class="info">
+  <p class="info-item">
+    <b>${likes}</b>
+  </p>
+  <p class="info-item">
+    <b>${views}</b>
+  </p>
+  <p class="info-item">
+    <b>${comments}</b>
+  </p>
+  <p class="info-item">
+    <b>${downloads}</b>
+  </p>
+</div>
+
+</div>
+`;
+  })
+  
+  .join('');
+  
+  refsEl.galleryContainer.insertAdjacentHTML(
+    "beforeend", result);
+    gallery.refresh()
 };
 
 
 
 
 
-function createImagesCardsMarkup(galleryItems) {
-  return galleryItems
-    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-      return `
-       <div class="photo-card">
-      <img href="${webformatURL}" src="${largeImageURL}" alt="${tags }" loading="lazy" />
-      <div class="info">
-        <p class="info-item">
-          <b>${likes}</b>
-        </p>
-        <p class="info-item">
-          <b>${views}</b>
-        </p>
-        <p class="info-item">
-          <b>${comments}</b>
-        </p>
-        <p class="info-item">
-          <b>${downloads}</b>
-        </p>
-      </div>
-    </div>
-       `;
-    })
-    .join("");
-}
 
 
-refsEl.getBtn.addEventListener("click", getPosts);
-refsEl.galleryContainer.insertAdjacentHTML(
-  "beforeend",
-  createImagesCardsMarkup(galleryItems)
-);
+
+
